6c36dd15fbc10f1eb8c33060419935bec5ea575b
Teach210 <sss20802@email.vccs.edu>
2024-05-08 13:10:32 -0400

    completed part 1 Binary_Search

--- Binary_Search_Project.cpp
+++ Binary_Search_Project.cpp
@@ -1,20 +1,109 @@
 // Binary_Search_Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+//#include <iostream>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+// Recursive Binary Search
+int recursiveBinarySearch(const vector<int>& arr, int left, int right, int target) {
+    if (right >= left) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] > target)
+            return recursiveBinarySearch(arr, left, mid - 1, target);
+
+        return recursiveBinarySearch(arr, mid + 1, right, target);
+    }
+    return -1;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+// Iterative Binary Search
+int iterativeBinarySearch(const vector<int>& arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] == target)
+            return mid;
+
+        if (arr[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+
+// Sequential Search
+int sequentialSearch(const vector<int>& arr, int target) {
+    for (int i = 0; i < arr.size(); ++i) {
+        if (arr[i] == target)
+            return i;
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> data = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
+    int target1 = 10; // target that exists in the list
+    int target2 = 5;  // target that does not exist in the list
+
+    // Sorting the vector
+    sort(data.begin(), data.end());
+
+    // Recursive Binary Search
+    int index = recursiveBinarySearch(data, 0, data.size() - 1, target1);
+    cout << "Contents of vector: ";
+    for (int num : data)
+        cout << num << " ";
+    cout << endl;
+    if (index != -1)
+        cout << "Target " << target1 << " found at location " << index << endl;
+    else
+        cout << "Target " << target1 << " was not found." << endl;
+
+    index = recursiveBinarySearch(data, 0, data.size() - 1, target2);
+    if (index != -1)
+        cout << "Target " << target2 << " found at location " << index << endl;
+    else
+        cout << "Target " << target2 << " was not found." << endl;
+
+    // Iterative Binary Search
+    index = iterativeBinarySearch(data, target1);
+    if (index != -1)
+        cout << "Target " << target1 << " found at location " << index << endl;
+    else
+        cout << "Target " << target1 << " was not found." << endl;
+
+    index = iterativeBinarySearch(data, target2);
+    if (index != -1)
+        cout << "Target " << target2 << " found at location " << index << endl;
+    else
+        cout << "Target " << target2 << " was not found." << endl;
+
+    // Sequential Search
+    index = sequentialSearch(data, target1);
+    if (index != -1)
+        cout << "Target " << target1 << " found at location " << index << endl;
+    else
+        cout << "Target " << target1 << " was not found." << endl;
+
+    index = sequentialSearch(data, target2);
+    if (index != -1)
+        cout << "Target " << target2 << " found at location " << index << endl;
+    else
+        cout << "Target " << target2 << " was not found." << endl;
+
+    return 0;
+}
+
+
+
+
